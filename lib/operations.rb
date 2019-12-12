de8273b016def22990e81207f7655d02a1d97c1f
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-12 22:26:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,18 @@
 def unsafe?(speed)
   
-  if speed < 40 || speed > 60
-    
-  else
-    false
+    if speed > 40 || speed > 60
+      puts "true"
+    else
+      puts "false"
+    end
 
 end
 
 
-
-def not_safe?(speed)
-	
-end
-	
-
-
+  unsafe?(50)
+  
+  
+  def not_safe?(speed)
+      
+  end
+      
