cf21731f919646f4fe3934e105270f551b402af8
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-12 22:35:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,7 +12,7 @@ end
   
   def not_safe?(speed)
     
-  speed < 40 || speed > 60 ? puts "this is unsafe" : puts " this is safe"  
+  speed < 40 || speed > 60 ? puts "this is unsafe" : puts "this is safe"  
 
   end
   
